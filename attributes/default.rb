7441d7b59c6637b8153cd1d639d676d256da9755
robzor92 <robin.eric.andersson@gmail.com>
2018-05-16 20:03:20 +0200

    [HOPSWORKS-558] Update ELK stack to 6.2.4

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,7 @@ default['hopslog']['dir']                       = node['install']['dir'].empty? 
 #
 # Logstash
 #
-default['logstash']['version']                  = "2.4.1"
+default['logstash']['version']                  = "6.2.3"
 default['logstash']['url']                      = "#{node['download_url']}/logstash-#{node['logstash']['version']}.tar.gz"
 default['logstash']['http']['port']             = "3456"
 
@@ -23,7 +23,7 @@ default['logstash']['pid_file']                 = "/tmp/logstash.pid"
 #
 # Kibana
 #
-default['kibana']['version']                    = "4.6.4"
+default['kibana']['version']                    = "6.2.3"
 default['kibana']['url']                        = "#{node['download_url']}/kibana-#{node['kibana']['version']}-linux-x86_64.tar.gz"
 default['kibana']['port']                       = "5601"
 
@@ -37,7 +37,7 @@ default['kibana']['default_index']		  = "hopsdefault"
 #
 # Filebeat
 
-default['filebeat']['version']                  = "5.2.0"
+default['filebeat']['version']                  = "6.2.3"
 default['filebeat']['url']                      = "#{node['download_url']}/filebeat-#{node['filebeat']['version']}-linux-x86_64.tar.gz"
 default['filebeat']['home']                     = node['hopslog']['dir'] + "/filebeat-" + "#{node['filebeat']['version']}-linux-x86_64"
 default['filebeat']['base_dir']                 = node['hopslog']['dir'] + "/filebeat"
